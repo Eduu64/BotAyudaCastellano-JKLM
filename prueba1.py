c7725482f8a7ab26a650091fcff924c16ec5393a
Eduu64 <64559740+Eduu64@users.noreply.github.com>
2023-04-08 23:33:52 +0200

    v2

--- prueba1.py
+++ prueba1.py
@@ -2,6 +2,7 @@ from pynput.keyboard import Listener, Key
 import pyautogui
 import pyperclip
 import time
+import random
 
 
 with open('Diccionario.txt', encoding='utf-8') as diccionario:
@@ -10,23 +11,25 @@ with open('Diccionario.txt', encoding='utf-8') as diccionario:
 
 palabras_encontradas = []
 silaba = ""
+escritor = True
 
 def on_press(tecla):
 
-    global coordenada
+    global coordenada_x,coordenada_y
 
     if tecla == Key.ctrl_l:
-        coordenada = pyautogui.position()
+        coordenada_x,coordenada_y = pyautogui.position()
         #print(coordenada)
     
     if tecla == Key.alt_gr:
         # Mueve el cursor a la posición almacenada en la variable 'coordenada'
-        pyautogui.moveTo(coordenada)
+        pyautogui.moveTo(coordenada_x,coordenada_y)
         # Hace un doble clic en la posición actual del cursor
         pyautogui.doubleClick()
         #ctrl+c
         pyautogui.hotkey('ctrl', 'c')
         time.sleep(0.01)
+        pyautogui.moveTo(coordenada_x-100,coordenada_y)
         pyautogui.click()
         silaba = pyperclip.paste().lower().strip()
         #print(silaba)
@@ -36,9 +39,18 @@ def on_press(tecla):
         # Iterar sobre cada palabra en la línea
         for palabra in palabras:
              # Verificar si la sílaba está presente en la palabra
+             
              if silaba in palabra and palabra not in palabras_encontradas:
-                print(palabra)
-                pyperclip.copy(palabra)
+
+                #print(palabra)
+
+                if escritor:
+                    pyautogui.write(palabra)
+                    pyautogui.press('Enter')     
+                else:
+                     pyperclip.copy(palabra)
+
+               
                 palabras_encontradas.append(palabra) # Agregar la palabra al conjunto de palabras encontradas
                 break # Detener la búsqueda después de encontrar la primera palabra
 
